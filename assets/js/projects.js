cdce8c35525cdc9dbe07c74a3ea1e5be7d98881f
Kylian Bardini <kylianb1@icloud.com>
2022-08-23 14:02:47 +0200

    projects ajust

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -50,7 +50,7 @@ var projects = [
         name: "House & Garden",
         project: "portfolio3",
         type: "image",
-        img: "HGimg",
+        img: "HGimg.png",
         description: "Chartre graphique pour la société House and Garden",
         list: [
             "Photoshop",
